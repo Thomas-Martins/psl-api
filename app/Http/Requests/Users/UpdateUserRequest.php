805a026583adebf55543dc581c302f76b98b5111
Thomas Martins <thomas.martins1710@gmail.com>
2025-04-28 09:26:04 +0200

    feat: add user image upload functionality and update validation rules

--- app/Http/Requests/Users/UpdateUserRequest.php
+++ app/Http/Requests/Users/UpdateUserRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests\Users;
 
+use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateUserRequest extends FormRequest
 {
@@ -17,19 +19,34 @@ class UpdateUserRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, ValidationRule|array|string>
      */
     public function rules(): array
     {
+        $userId = $this->route('user')->id;
         return [
             'lastname' => ['sometimes', 'string', 'max:100'],
             'firstname' => ['sometimes', 'string', 'max:100'],
-            'email' => ['sometimes', 'string', 'email', 'max:255', 'exists:users,email'],
+            'email' => [
+                'sometimes',
+                'string',
+                'email',
+                'max:255',
+                Rule::unique('users', 'email')->ignore($userId),
+            ],
+
             'phone' => ['sometimes', 'string', 'max:20'],
             'address' => ['sometimes', 'string', 'max:255'],
             'zipcode' => ['sometimes', 'string', 'max:5'],
             'city' => ['sometimes', 'string', 'max:100'],
             'role_id' => ['sometimes', 'integer', 'exists:roles,id'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.unique' => 'Une erreur est survenue.',
         ];
     }
 }
