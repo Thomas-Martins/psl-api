121db6a951f4bec4acccc00d8d51d747909e5a45
Thomas Martins <thomas.martins1710@gmail.com>
2025-07-20 22:57:46 +0200

    feat: implement API authentication middleware and enhance exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Middleware\ApiAuthenticate;
 use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Log;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,10 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->alias(['roles' => RoleMiddleware::class]);
+        $middleware->alias(['roles' => RoleMiddleware::class, 'auth' => ApiAuthenticate::class,]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (\Illuminate\Auth\AuthenticationException $e, $request) {
+            Log::info('Handler called for unauthenticated request');
             return response()->json([
                 'message' => 'Unauthenticated.'
             ], 401);
